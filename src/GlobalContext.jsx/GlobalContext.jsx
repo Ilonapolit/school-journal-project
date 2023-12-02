0bb9949930a231515372e66338d3e3d826d3969b
Ilonapolit <139139277+Ilonapolit@users.noreply.github.com>
2023-12-02 11:09:06 +0400

    usestatecommit

--- src/GlobalContext.jsx/GlobalContext.jsx
+++ src/GlobalContext.jsx/GlobalContext.jsx
@@ -1,5 +1,5 @@
-import {createContext} from "react";
-const chapters = [
+import {createContext, useState} from "react";
+const chapters_default = [
     {
         id:"day-1",
         name:"ორშაბათი",
@@ -22,16 +22,21 @@ const chapters = [
     }
 ]
 
-const users = [
-    {id:"user-1", name:"ilona"},
-    {id:"user-2", name:"danny"}
-]
-const grades = [
-    {id:"grade-1",userId:"user-1",chapterId:"day-1",grade:40}
-]
+// const users = [
+//     {id:"user-1", name:"ilona"},
+//     {id:"user-2", name:"danny"}
+// ]
+// const grades = [
+//     {id:"grade-1",userId:"user-1",chapterId:"day-1",grade:40}
+// ]
 
 export const GlobalContext = createContext({});
 
 export function GlobalProvider({children}) {
+    const [chapters,setChapters]=useState(chapters_default);
+    const [users,setUsers]=useState([])
+const [grades,setGrades]=useState([]);
+
+
     return <GlobalContext.Provider>{children}</GlobalContext.Provider>;
 }
